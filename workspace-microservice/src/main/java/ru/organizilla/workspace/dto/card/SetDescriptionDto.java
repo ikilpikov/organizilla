941612f61c906515cdbdb257fb3e066b8daba735
Ilya Kilpikov <ikilpikovv@gmail.com>
2024-05-26 17:38:52 +0300

    Description can be set as null

--- workspace-microservice/src/main/java/ru/organizilla/workspace/dto/card/SetDescriptionDto.java
+++ workspace-microservice/src/main/java/ru/organizilla/workspace/dto/card/SetDescriptionDto.java
@@ -1,6 +1,5 @@
 package ru.organizilla.workspace.dto.card;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.Setter;
 @Setter
 public class SetDescriptionDto {
 
-    @NotNull
     @Size(min = 1, max = 512)
     private String description;
 }
